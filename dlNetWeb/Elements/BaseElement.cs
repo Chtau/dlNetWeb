61745cb3c566004cf930f7ad89ae3faf3a263cb9
Christoph Taucher <taucher.ch@gmail.com>
2021-04-22 19:51:27 +0200

    add elements

---
+++ dlNetWeb/Elements/BaseElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dlNetWeb.Elements
+{
+    // TODO: https://dom.spec.whatwg.org/#concept-element
+    public abstract class BaseElement
+    {
+        public string Namespace { get; set; }
+        public string NamespacePrefix {get;set;}
+        public string LocalName { get; set; }
+        public ElementCustomElementState CustomElementState { get; set; }
+        public string CustomElementDefinition { get; set; }
+        public bool IsValue { get; set; }
+        public bool Defined => CustomElementState == ElementCustomElementState.Uncustomized || CustomElementState == ElementCustomElementState.Custom;
+        public bool Custom => CustomElementState == ElementCustomElementState.Custom;
+    }
+}
---
+++ dlNetWeb/Elements/ElementEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dlNetWeb.Elements
+{
+    public enum ElementCustomElementState
+    {
+        Undefined,
+        Failed,
+        Uncustomized,
+        Precustomized,
+        Custom
+    }
+}
